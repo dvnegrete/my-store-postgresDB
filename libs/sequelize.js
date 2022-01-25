72dc545f1afc7690263340019f4e843add2f8d78
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2022-01-25 09:21:15 -0600

    Configuracion de migraciones Sequalize trabajaba, pero con error al ejecutar migrations run

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -6,17 +6,17 @@ const setupModels = require("./../db/models");
 
 const USER = encodeURIComponent(config.dbUsers);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI,{
-  dialect:"mysql",
-  login: true,
+  dialect:"postgres",
+  logging: true,
 });
 
 //aqui creamos la conexion a la BD
 setupModels(sequelize);
 
-//Sincronizamos. Cuando el modelo esta configurado el lee el schema y lo crea. Crea la tabla en e este caso
-sequelize.sync();
+//Sincronizamos. Cuando el modelo esta configurado el lee el schema y lo crea. Crea la tabla en e este caso. No se aconseja en produccion
+//sequelize.sync();
 
 module.exports = sequelize;
